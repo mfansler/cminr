f6b6d98a1ac879da394b494cdbb4549b17575218
Merv Fansler <mmfansler@gmail.com>
2014-09-19 23:35:16 -0400

    lexer functional; added example c- file

--- as4/CMinLexerDriver.cc
+++ as4/CMinLexerDriver.cc
@@ -20,14 +20,18 @@
 
 //**
 
-extern FILE* yyin;
-
 extern "C"
 int
 yylex ();
 
-//**
+extern FILE* yyin;
 
+extern char* yytext;
+
+extern int lineCount;
+extern int colCount;
+
+//**
 
 int
 main (int argc, char* argv[])
@@ -42,15 +46,13 @@ main (int argc, char* argv[])
     yyin = stdin;
   }
 
-  std::cout << IF;
+  int result;
 
-  int result;  // CHECK: Make sure this is correct; maybe retun a
-               // freakn' token straight away
   do
   {
     result = yylex ();
     switch (result)
-    { // TODO: fill out cases
+    { // TODO: DISPLAY LEXEMES AND VALUES; FORMAT
       case ERROR:
         printf("TokenError: Unrecognized character sequence");
         break;
@@ -77,6 +79,72 @@ main (int argc, char* argv[])
         break;
       case PLUS:
         printf("PLUS\n");
+        break;
+      case MINUS:
+        printf("MINUS\n");
+        break;
+      case TIMES:
+        printf("TIMES\n");
+        break;
+      case DIVIDE:
+        printf("DIVIDE\n");
+        break;
+      case LT:
+        printf("LT\n");
+        break;
+      case LTE:
+        printf("LTE\n");
+        break;
+      case GT:
+        printf("GT\n");
+        break;
+      case GTE:
+        printf("GTE\n");
+        break;
+      case EQ:
+        printf("EQ\n");
+        break;
+      case NEQ:
+        printf("NEQ\n");
+        break;
+      case INCREMENT:
+        printf("INCREMENT\n");
+        break;
+      case DECREMENT:
+        printf("DECREMENT\n");
+        break;
+      case ASSIGN:
+        printf("ASSIGN\n");
+        break;
+      case SEMI:
+        printf("SEMI\n");
+        break;
+      case COMMA:
+        printf("COMMA\n");
+        break;
+      case LPAREN:
+        printf("LPAREN\n");
+        break;
+      case RPAREN:
+        printf("RPAREN\n");
+        break;
+      case LBRACK:
+        printf("LBRACK\n");
+        break;
+      case RBRACK:
+        printf("RBRACK\n");
+        break;
+      case LBRACE:
+        printf("LBRACE\n");
+        break;
+      case RBRACE:
+        printf("RBRACE\n");
+        break;
+      case ID:
+        printf("ID\n");
+        break;
+      case NUM:
+        printf("NUM\n");
         break;
       default:
         printf("Token number %i",result);
