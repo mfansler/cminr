83a5f3a8cff4374766dc4839313c44409c424ce3
Merv Fansler <mmfansler@gmail.com>
2014-10-06 02:45:44 -0400

    rd parser rough draft

--- as5/CMinParser.cc
+++ as5/CMinParser.cc
@@ -60,16 +60,70 @@ void
 D2 ();
 
 void
-F ();
-
-void
-S1 ();
+U ();
 
 void
 P1 ();
 
 void
 P2 ();
+
+void
+Y ();
+
+void
+C ();
+
+void
+V ();
+
+void
+S ();
+
+void
+I ();
+
+void
+W ();
+
+void
+R ();
+
+void
+E ();
+
+void
+X ();
+
+void
+X2 ();
+
+void
+H ();
+
+void
+G ();
+
+void
+Q ();
+
+void
+T ();
+
+void
+F ();
+
+void
+F2 ();
+
+void
+L ();
+
+void
+A ();
+
+void
+M ();
 
 /******************************************************************************/
 // Global vars
@@ -141,19 +195,19 @@ P ()
 }
 
 /******************************************************************************/
-// D --> void id F | int id D2
+// D --> VOID ID U | INT ID D2
 
 void
 D ()
 {
   if (g_token == VOID)
-  { // must be F declaration
+  { // must be U (function) declaration
     match (VOID);
     match (ID);
-    F ();
+    U ();
   }
   else if (g_token == INT)
-  { // could still be F or V
+  { // could still be U or V
     match (INT);
     match (ID);
     D2 ();
@@ -163,14 +217,14 @@ D ()
 }
 
 /******************************************************************************/
-// D2 --> F | [ '[' num ']' ] ';'
+// D2 --> U | [ LBRACK NUM RBRACK ] SEMI
 
 void
 D2 ()
 {
   if (g_token == LPAREN)
   { // must be function declaration
-    F ();
+    U ();
   }
   else if (g_token == LBRACK)
   {
@@ -188,20 +242,19 @@ D2 ()
 }
 
 /******************************************************************************/
-// F --> id lparen P1 rparen S1
+// U --> LPAREN P RPAREN C
 
 void
-F ()
+U ()
 {
-  match (ID);
   match (LPAREN);
   P1 ();
   match (RPAREN);
-  S1 ();
+  C ();
 }
 
 /******************************************************************************/
-// P1 --> void | P2 { semi P2 }
+// P1 --> VOID | P2 { COMMA P2 }
 
 void
 P1 ()
@@ -211,9 +264,9 @@ P1 ()
   else if (g_token == INT)
   {
     P2 ();
-    while (g_token == SEMI)
+    while (g_token == COMMA)
     {
-      match (SEMI);
+      match (COMMA);
       P2 ();
     }
   }
@@ -222,12 +275,12 @@ P1 ()
 }
 
 /******************************************************************************/
-// P2 --> int id [ lbrack rbrack ]
+// P2 --> Y ID [ LBRACK RBRACK ]
 
 void
 P2 ()
 {
-  match (INT);
+  Y ();
   match (ID);
   if (g_token == LBRACK)
   {
@@ -237,10 +290,354 @@ P2 ()
 }
 
 /******************************************************************************/
-// S1 -->
+// Y --> VOID | INT
+void
+Y ()
+{
+  if (g_token == VOID)
+    match (VOID);
+  else if (g_token == INT)
+    match (INT);
+  else
+    error ("Y, type unspecified");
+}
+
+
+/******************************************************************************/
+// C --> LBRACE V { S } RBRACE
 
 void
-S1 ()
+C ()
 {
+  match (LBRACE);
+  V ();
+  while (g_token != RBRACE)
+    S ();
+  match (RBRACE);
+}
 
+/******************************************************************************/
+// V --> { INT ID [ LBRACK NUM RBRACK ] SEMI }
+
+void
+V ()
+{
+  while (g_token == INT)
+  {
+    match (INT);
+    match (ID);
+    if (g_token == LBRACK)
+    {
+      match (LBRACK);
+      match (NUM);
+      match (RBRACK);
+    }
+    match (SEMI);
+  }
+}
+
+/******************************************************************************/
+// S --> C | I | W | R | E
+
+void
+S ()
+{
+  if (g_token == LBRACE)
+    C ();
+  else if (g_token == IF)
+    I ();
+  else if (g_token == WHILE)
+    W ();
+  else if (g_token == RETURN)
+    R ();
+  else
+    E ();
+}
+
+/******************************************************************************/
+
+void
+I ()
+{
+  match (IF);
+  match (LPAREN);
+  X ();
+  match (RPAREN);
+  S ();
+  if (g_token == ELSE)
+  {
+    match (ELSE);
+    S ();
+  }
+}
+
+/******************************************************************************/
+// W --> WHILE LPAREN X RPAREN S
+
+void
+W ()
+{
+  match (WHILE);
+  match (LPAREN);
+  X ();
+  match (RPAREN);
+  S ();
+}
+
+/******************************************************************************/
+// R --> RETURN [ X ] SEMI
+
+void
+R ()
+{
+  match (RETURN);
+  if (g_token != SEMI)
+    X ();
+  match (SEMI);
+}
+
+/******************************************************************************/
+// E --> [ X ] SEMI
+
+void
+E ()
+{
+  if (g_token != SEMI)
+    X ();
+  match (SEMI);
+}
+
+/******************************************************************************/
+// X --> NUM H | LPAREN X RPAREN H | ID X2
+
+void
+X ()
+{
+  if (g_token == NUM)
+  {
+    match (NUM);
+    H ();
+  }
+  else if (g_token == LPAREN)
+  {
+    match (LPAREN);
+    X ();
+    match (RPAREN);
+    H ();
+  }
+  else if (g_token == ID)
+  {
+    match (ID);
+    X2 ();
+  }
+  else
+    error ("X, invalid expression");
+}
+
+/******************************************************************************/
+// X2 --> LPAREN G RPAREN H | ASSIGN X | [ LBRACK X RBRACK ] H
+
+void
+X2 ()
+{
+  if (g_token == LPAREN)
+  {
+    match (LPAREN);
+    G ();
+    match (RPAREN);
+    H ();
+  }
+  else if (g_token == ASSIGN)
+  {
+    match (ASSIGN);
+    X ();
+  }
+  else
+  {
+    if (g_token == LBRACK)
+    {
+      match (LBRACK);
+      X ();
+      match (RBRACK);
+    }
+    H ();
+  }
+}
+
+/******************************************************************************/
+// H --> { M T } { A Q } { L Q }
+
+void
+H ()
+{
+  while (g_token == TIMES || g_token == DIVIDE)
+  {
+    M ();
+    T ();
+  }
+  while (g_token == PLUS || g_token == MINUS)
+  {
+    A ();
+    Q ();
+  }
+  while (g_token == LT || g_token == LTE || g_token == EQ ||
+	 g_token == GT || g_token == GTE || g_token == NEQ)
+  {
+    L ();
+    Q ();
+  }
+}
+
+/******************************************************************************/
+// G --> [ X { COMMA X } ]
+
+void
+G ()
+{
+  if (g_token == NUM || g_token == LPAREN || g_token == ID)
+  {
+    X ();
+    while (g_token == COMMA)
+    {
+      match (COMMA);
+      X ();
+    }
+  }
+  else if (g_token == RPAREN)
+  {
+    // empty
+  }
+  else
+    error ("G, argument syntax invalid");
+}
+
+/******************************************************************************/
+// Q --> T { A Q }
+
+void
+Q ()
+{
+  T ();
+  while (g_token == PLUS || g_token == MINUS)
+  {
+    A ();
+    Q ();
+  }
+}
+
+/******************************************************************************/
+// T --> F { M T }
+
+void
+T ()
+{
+  F ();
+  while (g_token == TIMES || g_token == DIVIDE)
+  {
+    M ();
+    T ();
+  }
+}
+
+/******************************************************************************/
+// F -> LPAREN X RPAREN | NUM | ID F2
+
+void
+F ()
+{
+  if (g_token == LPAREN)
+  {
+    match (LPAREN);
+    X ();
+    match (RPAREN);
+  }
+  else if (g_token == NUM)
+    match (NUM);
+  else if (g_token == ID)
+  {
+    match (ID);
+    F2 ();
+  }
+  else
+    error ("F, missing factor");
+}
+
+/******************************************************************************/
+// F2 --> LPAREN args RPAREN | [ LBRACK X RBRACK ]
+
+void
+F2 ()
+{
+  if (g_token == LPAREN)
+  {
+    match (LPAREN);
+    G ();
+    match (RPAREN);
+  }
+  else if (g_token == LBRACK)
+  {
+    match (LBRACK);
+    X ();
+    match (RBRACK);
+  }
+  // else => empty
+}
+
+/******************************************************************************/
+// L --> LT | LTE | GT | GTE | EQ | NEQ
+
+void
+L ()
+{
+  switch (g_token)
+  {
+  case LT:
+    match (LT);
+    break;
+  case LTE:
+    match (LTE);
+    break;
+  case GT:
+    match (GT);
+    break;
+  case GTE:
+    match (GTE);
+    break;
+  case EQ:
+    match (EQ);
+    break;
+  case NEQ:
+    match (NEQ);
+    break;
+  default:
+    error ("L, Logical operation missing");
+  }
+}
+
+/******************************************************************************/
+// A --> PLUS | MINUS
+
+void
+A ()
+{
+  if (g_token == PLUS)
+    match (PLUS);
+  else if (g_token == MINUS)
+    match (MINUS);
+  else
+    error ("A, Additive operator missing");
+}
+
+/******************************************************************************/
+// M --> TIMES | DIVIDE
+
+void
+M ()
+{
+  if (g_token == TIMES)
+    match (TIMES);
+  else if (g_token == DIVIDE)
+    match (DIVIDE);
+  else
+    error ("M, Multiplicative operator missing");
 }
