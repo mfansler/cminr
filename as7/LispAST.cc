642330a675dbf7e7b95542999eade6cd5f78b01e
Merv Fansler <mmfansler@gmail.com>
2014-10-13 00:02:24 -0400

    lisperer functional

--- as7/LispAST.cc
+++ as7/LispAST.cc
@@ -19,22 +19,133 @@
 #include <cctype>
 
 /****************************************************/
+// Class definitions
+
+class TreeNode
+{ // abstract
+  protected:
+    TreeNode* child = NULL;
+    TreeNode* sibling = NULL;
+  
+  public:
+    virtual int
+    evaluate () {};
+
+    void
+    setSibling (TreeNode* s)
+    {
+      sibling = s;
+    };
+
+    TreeNode*
+    getSibling ()
+    {
+      return sibling;
+    };
+
+    void
+    setChild (TreeNode* c)
+    {
+      child = c;
+    };
+
+    TreeNode*
+    getChild ()
+    {
+      return child;
+    };
+};
+
+class NumberNode: public TreeNode
+{
+  int value;
+  
+  public:
+    NumberNode (int v)
+    {
+      value = v;
+    }
+
+    int
+    evaluate ()
+    {
+      return value;
+    };
+};
+
+class AddNode: public TreeNode
+{
+  public:
+    int
+    evaluate ()
+    {
+      int sum = child->evaluate ();
+      TreeNode* next = child->getSibling ();
+      while (next != NULL)
+      {
+	sum += next->evaluate ();
+	next = next->getSibling ();
+      }
+      return sum;
+    };
+};
+
+class TimesNode: public TreeNode
+{
+  public:
+    int
+    evaluate ()
+    {
+      int product = child->evaluate ();
+      TreeNode* next = child->getSibling ();
+      while (next != NULL || product == 0)
+      {
+	product *= next->evaluate ();
+	next = next->getSibling ();
+      }
+      return product;
+    };
+};
+
+class SubtractNode: public TreeNode
+{
+  public:
+    int
+    evaluate ()
+    {
+      int difference = child->evaluate ();
+      
+      TreeNode* next = child->getSibling ();
+
+      if (next == NULL)
+	return -difference;
+      
+      while (next != NULL)
+      {
+	difference -= next->evaluate ();
+	next = next->getSibling ();
+      }
+      return difference;
+    };
+};
+
+/****************************************************/
 // Function declarations
 
 TreeNode*
 parse ();
 
-TreeNode *
+TreeNode*
 lexp ();
 
-TreeNode *
+TreeNode*
 op ();
 
-TreeNode *
-lexp-seq ();
+TreeNode*
+lexpSeq ();
 
 void
-match (char token, const char* callee);
+match (char token, const char* error);
 
 void
 error (const char* error);
@@ -52,85 +163,120 @@ char g_token;
 int
 main ()
 {
-  g_token = getToken ();
-  Tree t = parse ();
+  TreeNode* t = parse ();
+
+  printf ("Valid LISP expression.\nValue = %d\n", t->evaluate ());
   
   return EXIT_SUCCESS;
 }
 
 /****************************************************/
 
-TreeNode *
+TreeNode*
 parse ()
 {
-  TreeNode * t;
+  TreeNode* t;
+  
   g_token = getToken ();
   t = lexp ();
-  if (token != EOF)
-    error ("parse");
+  if (g_token != EOF)
+    error ("parse, trailing characters found");
 
   return t;
 }
 
 /****************************************************/
 
-TreeNode *
+TreeNode*
 lexp ()
 {
   // To handle production
-  // lexp --> number | ( op lexp-seq )
+  // lexp --> number | ( op lexpSeq )
 
-  TreeNode * t;
+  TreeNode* t;
   
   if (isdigit (g_token))
   {
     int num;
-    putback (g_token);
-    std::cin >> num;
+    ungetc (g_token, stdin);
+    scanf ("%d", &num);
     
-    t = newExpNode (NumberK);
-    t.value = num;
+    t = new NumberNode (num);
+
+    g_token = getToken ();
   }
   else if (g_token == '(')
   {
-    match ('(');
-    t = newOpNode ();
-
+    match ('(', "lexp");
+    t = op ();
+    t->setChild (lexpSeq ());
+    match (')', "lexp");
   }
   else
-    error ("lexp");
+    error ("lexp, invalid start of expression");
 
   return t;  
 }
 
 /****************************************************/
 
-TreeNode *
+TreeNode*
 op ()
 {
   // To handle production
-  // op -->
+  // op --> + | - | *
+  
+  TreeNode* t;
 
-  TreeNode * t;
+  switch (g_token)
+  {
+    case '*':
+      match ('*', "op");
+      t = new TimesNode ();
+      break;
+    case '+':
+      match ('+', "op");
+      t = new AddNode ();
+      break;
+    case '-':
+      match ('-', "op");
+      t = new SubtractNode ();
+      break;
+    default:
+      error ("op, operator missing");
+  }
   
   return t;  
 }
 
 /****************************************************/
 
-TreeNode *
-lexp-seq ()
+TreeNode*
+lexpSeq ()
 {
   // To handle production
-  // lexp-seq -->
-
-  TreeNode * t;
+  // lexpSeq --> lexp { lexp }
+  
+  TreeNode* t;
+  if (g_token == '(' || isdigit (g_token))
+  {
+    t = lexp ();
+    TreeNode* next = t;
+    while (g_token == '(' || isdigit (g_token))
+    {
+      next->setSibling (lexp ());
+      next = next->getSibling ();
+    }
+  }
+  else
+    error ("lexpSeq, invalid expression start");
   
   return t;  
 }
 
 /****************************************************/
 // matches token and advances input
+
 void
 match (char token, const char* callee)
 {
